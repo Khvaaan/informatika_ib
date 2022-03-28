d00cf38010522e012b128811247b8dcb06a7bcab
Vladislav Khvan <khvaaan.study@gmail.com>
2022-03-28 11:10:32 +1000

    final_comm

--- main.cpp
+++ main.cpp
@@ -1,20 +1,29 @@
 #include <iostream>
 using namespace std;
 
+
 int multi(int x, int y){
 	return x*y;
 }
 
 int boner(int x){
 	return x*x;
+}
 
-int minus(int x, int y)
-{
+int diff(int x, int y){
 	return x-y;	
 }
 
 
-int plus(int x, int y)
-{
+int summ(int x, int y){
 	return x+y;
+}
+
+int main(){
+	int a, b;
+	cin >> a >> b;
+	cout << multi(a, b) << endl;
+	cout << boner(a) << endl;
+	cout << diff(a, b) << endl;
+	cout << summ(a, b) << endl;
 }
